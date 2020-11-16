ade6954b31a14fdce8861d8f4ac1eef4243a5a07
Sridhar Ratnakumar <srid@srid.ca>
2020-11-16 12:18:34 -0500

    Add gitignoresrc and cabal-fmt

--- default.nix
+++ default.nix
@@ -1,10 +1,16 @@
 { pkgs ? import ./dep/nixpkgs {} }:
-pkgs.haskellPackages.developPackage {
-root = ./.;
-modifier = drv:
-    pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-    [ cabal-install
-        ghcid
-        haskell-language-server
-    ]);
-}
+
+let 
+  inherit (import ./dep/gitignore { inherit (pkgs) lib; }) gitignoreSource;
+in 
+  pkgs.haskellPackages.developPackage {
+    name = "pandoc-link-context";
+    root = gitignoreSource ./.;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [ cabal-install
+          cabal-fmt
+          ghcid
+          haskell-language-server
+        ]);
+  }
